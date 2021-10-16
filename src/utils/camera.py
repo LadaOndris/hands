4724658a072c9a9b40f2346ebda85f911aaf3fac
Ladislav Ondris <ladislav.ondris@seznam.cz>
2021-10-16 16:28:10 +0200

    Introduced inheritance for camera classes

--- src/utils/camera.py
+++ src/utils/camera.py
@@ -1,33 +1,31 @@
-import tensorflow as tf
+from typing import Tuple
 
-from src.utils.debugging import timing
+import numpy as np
+import tensorflow as tf
 
 
 class Camera:
 
-    def __init__(self, cam_type: str):
-        self.dataset = cam_type
-        self.extrinsic_matrix = tf.eye(4)
-        self.focal_length = None
-        self.principal_point = None
-        self.image_size = None
-        self.intrinsic_matrix = None
-        self.projection_matrix = None
-        self.invr_projection_matrix = None
-        self.depth_unit = None
+    def __init__(self,
+                 focal_length: Tuple[float, float],
+                 principal_point: Tuple[float, float],
+                 image_size: Tuple[int, int],
+                 extrinsic_matrix: np.ndarray = np.eye(4),
+                 depth_unit: float = None):
+        self.focal_length = focal_length
+        self.principal_point = principal_point
+        self.image_size = image_size
+        self.depth_unit = depth_unit
+        self.extrinsic_matrix = tf.constant(extrinsic_matrix, dtype=tf.float32)
 
-        cam_type = cam_type.lower()
-        if cam_type == 'bighand':
-            self.set_bighand_sr300()
-        elif cam_type == 'msra':
-            self.set_msra_creative_interactive()
-        elif cam_type == 'custom' or cam_type == 'sr305':
-            self.set_sr305()
-        elif cam_type == 'd415':
-            self.set_d415()
-        else:
-            raise NotImplementedError('Unknown dataset')
-        self.create_projection_matrices()
+        self.intrinsic_matrix = tf.constant([[self.focal_length[0], 0, self.principal_point[0], 0],
+                                             [0, self.focal_length[1], self.principal_point[1], 0],
+                                             [0, 0, 1, 0],
+                                             [0, 0, 0, 1]], dtype=tf.float32)
+        # World_to_pixel projection matrix
+        self.projection_matrix = tf.matmul(self.intrinsic_matrix, self.extrinsic_matrix)
+        # Pixel_to_world projection matrix
+        self.invr_projection_matrix = tf.linalg.inv(self.projection_matrix)
 
     def set_msra_creative_interactive(self):
         self.focal_length = [241.42, 241.42]
@@ -74,13 +72,7 @@ class Camera:
              [0, 0, 1, 3.902],
              [0, 0, 0, 1]], dtype=tf.float32)
         """
-        self.intrinsic_matrix = tf.constant([[self.focal_length[0], 0, self.principal_point[0], 0],
-                                             [0, self.focal_length[1], self.principal_point[1], 0],
-                                             [0, 0, 1, 0],
-                                             [0, 0, 0, 1]], dtype=tf.float32)
-        # Compute WorldToPixel projection matrix
-        self.projection_matrix = tf.matmul(self.intrinsic_matrix, self.extrinsic_matrix)
-        self.invr_projection_matrix = tf.linalg.inv(self.projection_matrix)
+        raise RuntimeError('Not supported, deprecated')
 
     def world_to_pixel(self, coords_xyz):
         """
@@ -184,3 +176,38 @@ class Camera:
         tranposed_xyz = tf.matmul(self.invr_projection_matrix, multiplied_uvz1, transpose_b=True)
         xyz = tf.transpose(tranposed_xyz, [0, 2, 1])[..., :3]
         return xyz
+
+
+class CameraBighand(Camera):
+
+    def __init__(self):
+        super().__init__(focal_length=(475.065948, 475.065857),
+                         principal_point=(315.944855, 245.287079),
+                         image_size=(640, 480),
+                         depth_unit=0.001,
+                         extrinsic_matrix=
+                         np.array([[0.999988496304, -0.00468848412856, 0.000982563360594, 0],
+                                   [0.00469115935266, 0.999985218048, -0.00273845880292, 0],
+                                   [-0.000969709653873, 0.00274303671904, 0.99999576807, 0],
+                                   [0, 0, 0, 1]]))
+        # self.focal_length = [476.0068, 476.0068]  # [588.235, 587.084]
+
+
+class CameraSR305(Camera):
+    super().__init__(focal_length=(476.0068, 476.0068),
+                     principal_point=(313.6830139, 242.7547302),
+                     image_size=(640, 480),
+                     depth_unit=0.000125)
+
+
+class CameraD105(Camera):
+    super().__init__(focal_length=(592.138, 592.138),
+                     principal_point=(313.79, 238.076),
+                     image_size=(640, 480),
+                     depth_unit=0.001)
+
+
+class CameraMSRA(Camera):
+    super().__init__(focal_length=(241.42, 241.42),
+                     principal_point=(160, 120),
+                     image_size=(320, 240))
